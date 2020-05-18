bc03b01ab620c27ec0d92651c02626690156019c
KonradP <konradpodstawskii@gmail.com>
2020-05-18 19:28:06 +0200

    add blending

--- src/Engine/mod.rs
+++ src/Engine/mod.rs
@@ -1,6 +1,5 @@
 extern crate ugli_webgl;
 
-#[macro_use]
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -9,7 +8,7 @@ use stdweb::web::{document, window, IEventTarget, IHtmlElement, IParentNode, Typ
 
 use stdweb::web::html_element::ImageElement;
 
-use stdweb::web::event::{IEvent, IKeyboardEvent, KeyDownEvent, KeyboardLocation, ResizeEvent};
+use stdweb::web::event::ResizeEvent;
 
 use stdweb::web::html_element::CanvasElement;
 
@@ -36,27 +35,39 @@ struct Engine {
     v_matrix: WebGLUniformLocation,
     m_matrix: WebGLUniformLocation,
     index_buffer: WebGLBuffer,
+    test_one: i32,
     // TESTOWE ZMIENNE
     url: &'static str,
 }
 
 trait BasicEngine<T> {
     fn init(&mut self, _: T);
-    fn update();
+    fn update(&mut self, _: T);
     fn draw();
 }
 
 impl BasicEngine<Rc<RefCell<Self>>> for Engine {
     fn init(&mut self, _rc: Rc<RefCell<Self>>) {
-        self.context.enable(gl::DEPTH_TEST);
-        self.context.depth_func(gl::LEQUAL);
+        self.context.enable(gl::BLEND);
+        self.context.blend_func(gl::ONE, gl::ONE_MINUS_SRC_ALPHA);
+        //self.context.blend_func_separate(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA, gl::ONE, gl::ONE_MINUS_SRC_ALPHA);
+        //self.context.depth_func(gl::LEQUAL);
         self.context.clear_color(0.5, 0.5, 0.5, 0.9);
-        self.context.clear_depth(1.0);
+        //self.context.clear_depth(1.0);
 
+        // //==============================================TEST================================================//
+
+        //==================================================================================================//
+    }
+
+    fn update(&mut self, _rc: Rc<RefCell<Self>>) {
         let (w, h) = (self.canvas.width(), self.canvas.height());
         let proj_matrix = get_projection(20., (w as f32) / (h as f32), 1., 100.);
 
-        self.context.viewport(-500, -150, w as i32, h as i32);
+        self.test_one -= 1;
+
+        self.context
+            .viewport(-self.test_one, -150, w as i32, h as i32);
 
         self.context
             .clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
@@ -70,12 +81,6 @@ impl BasicEngine<Rc<RefCell<Self>>> for Engine {
             .bind_buffer(gl::ELEMENT_ARRAY_BUFFER, Some(&self.index_buffer));
         self.context
             .draw_elements(gl::TRIANGLES, 6, gl::UNSIGNED_SHORT, 0);
-
-        window().request_animation_frame(move |time| {
-            _rc.borrow_mut().init(_rc.clone());
-        });
-
-        //==============================================TEST================================================//
 
         let ref tex = self.context.create_texture();
         self.context.bind_texture(gl::TEXTURE_2D, tex.as_ref());
@@ -104,10 +109,10 @@ impl BasicEngine<Rc<RefCell<Self>>> for Engine {
         self.context
             .tex_parameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
 
-        //==================================================================================================//
+        window().request_animation_frame(move |_time| {
+            _rc.borrow_mut().update(_rc.clone());
+        });
     }
-
-    fn update() {}
 
     fn draw() {}
 }
@@ -127,7 +132,7 @@ pub fn init() {
     canvas.set_width(canvas.offset_width() as u32);
     canvas.set_height(canvas.offset_height() as u32);
 
-    context.clear_color(1.0, 0.0, 0.0, 1.0);
+    context.clear_color(1.0, 1.0, 0.0, 1.0);
     context.clear(gl::COLOR_BUFFER_BIT);
 
     window().add_event_listener(enclose!( (canvas) move |_: ResizeEvent| {
@@ -144,12 +149,16 @@ pub fn init() {
 
     let indices = TypedArray::<u16>::from(&[0, 1, 2, 0, 2, 3][..]).buffer();
 
-    let textureCoordinates =
+    let texture_coordinates =
         TypedArray::<f32>::from(&[1., 0., 0., 1., 1., 0., 0., 1., 0., 0., 0., 0.][..]).buffer();
 
-    let textureCoordBuffer = context.create_buffer().unwrap();
-    context.bind_buffer(gl::ARRAY_BUFFER, Some(&textureCoordBuffer));
-    context.buffer_data_1(gl::ARRAY_BUFFER, Some(&textureCoordinates), gl::STATIC_DRAW);
+    let texture_coord_buffer = context.create_buffer().unwrap();
+    context.bind_buffer(gl::ARRAY_BUFFER, Some(&texture_coord_buffer));
+    context.buffer_data_1(
+        gl::ARRAY_BUFFER,
+        Some(&texture_coordinates),
+        gl::STATIC_DRAW,
+    );
 
     // Create and store data into vertex buffer
     let vertex_buffer = context.create_buffer().unwrap();
@@ -218,7 +227,7 @@ pub fn init() {
     let m_matrix = context
         .get_uniform_location(&shader_program, "Mmatrix")
         .unwrap();
-    let textures = context
+    let _textur = context
         .get_uniform_location(&shader_program, "tex")
         .unwrap();
 
@@ -235,7 +244,7 @@ pub fn init() {
     // Color
     context.enable_vertex_attrib_array(color);
 
-    context.bind_buffer(gl::ARRAY_BUFFER, Some(&textureCoordBuffer));
+    context.bind_buffer(gl::ARRAY_BUFFER, Some(&texture_coord_buffer));
     let uv = context.get_attrib_location(&shader_program, "a_uv") as u32;
     context.vertex_attrib_pointer(uv, 3, gl::FLOAT, false, 0, 0);
     context.enable_vertex_attrib_array(uv);
@@ -252,7 +261,7 @@ pub fn init() {
     // translating z
     view_matrix[14] -= 8.; //zoom
 
-    let url = "sprite.bmp";
+    let url = "sprite.png";
 
     let state = Rc::new(RefCell::new(Engine {
         mov_matrix,
@@ -263,10 +272,12 @@ pub fn init() {
         v_matrix,
         m_matrix,
         index_buffer,
+        test_one: 500,
         url,
     }));
 
     state.borrow_mut().init(state.clone());
+    state.borrow_mut().update(state.clone());
 
     // context
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,17 @@
 extern crate ugli_webgl;
 
-#[macro_use]
-extern crate stdweb;
 extern crate serde_derive;
+extern crate stdweb;
 extern crate stdweb_derive;
 
-use std::cell::RefCell;
-use std::rc::Rc;
-
-use stdweb::unstable::TryInto;
-use stdweb::web::{document, window, IEventTarget, IHtmlElement, IParentNode, TypedArray};
-
-use stdweb::web::html_element::ImageElement;
-
-use stdweb::web::event::{IEvent, IKeyboardEvent, KeyDownEvent, KeyboardLocation, ResizeEvent};
-
-use stdweb::web::html_element::CanvasElement;
-
-use ugli_webgl::WebGL2RenderingContext as gl;
-use ugli_webgl::WebGLBuffer;
-
-use ugli_webgl::WebGLUniformLocation;
-
-mod Engine;
-mod Shaders;
+mod engine;
+// mod Shaders;
 pub mod matrix;
 
 fn main() {
-    Engine::init();
+    engine::init();
 
     //Shaders::init_shader(_ctx, _canvas);
 
-    Engine::end();
+    engine::end();
 }
